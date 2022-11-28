518230816a4756c67bf96978950d9f64ddeb6efb
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-11-28 18:05:50 -0300

    feat: create updateuser

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,6 +1,7 @@
 import { IUserEntity } from '../entities/user.entity';
 import { UserDto } from './dto/userInput.dto';
 import { randomUUID } from 'node:crypto';
+import { PartialUserDto } from './dto/partialUserInput.dto';
 
 export class UserService {
   private users: IUserEntity[] = [];
@@ -9,5 +10,16 @@ export class UserService {
     const userEntity = { ...user, id: randomUUID() };
     this.users.push(userEntity);
     return userEntity;
+  }
+
+  async updateUser(userData: PartialUserDto): Promise<IUserEntity> {
+    this.users.map((user, index) => {
+      if (user.id === userData.id) {
+        const updateUser = Object.assign(user, userData);
+        this.users.splice(index, 1, updateUser);
+      }
+    });
+    const updateUser = this.users.find((user) => user.id === userData.id);
+    return updateUser;
   }
 }
