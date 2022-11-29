e9204cb435e7064c684cab947ce2ae6d07103f7b
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-11-29 08:30:02 -0300

    feat class validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // statuscode
+  app.useGlobalPipes(new ValidationPipe());
+
+  // swagger
   const config = new DocumentBuilder()
     .setTitle('API 3D Print')
     .setDescription('The API for 3d print products')
