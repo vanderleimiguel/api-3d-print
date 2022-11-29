a6fa66c68d3966743cabae79243d3f0f7ecfa478
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-11-29 09:32:23 -0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './prisma/database.module';
+import { ProductRepository } from './product/product.repository';
+import { ProductService } from './product/services/product.service';
 import { UserService } from './user/services/user.service';
 import { UserController } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
@@ -7,6 +9,6 @@ import { UserRepository } from './user/user.repository';
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, ProductService, ProductRepository],
 })
 export class AppModule {}
