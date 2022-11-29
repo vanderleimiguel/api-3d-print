bd3c2854a0f2e62abef6b2266d01979b73553fb1
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-11-28 23:59:40 -0300

    feat: refactoring with prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './prisma/database.module';
 import { UserService } from './user/services/user.service';
 import { UserController } from './user/user.controller';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -2,49 +2,38 @@ import { IUserEntity } from '../entities/user.entity';
 import { UserDto } from './dto/userInput.dto';
 import { randomUUID } from 'node:crypto';
 import { PartialUserDto } from './dto/partialUserInput.dto';
+import { UserRepository } from '../user.repository';
 
 export class UserService {
-  private users: IUserEntity[] = [];
+  constructor(private readonly userRepository: UserRepository) {}
 
   async createUser(user: UserDto): Promise<IUserEntity> {
     const userEntity = { ...user, id: randomUUID() };
-    this.users.push(userEntity);
-    return userEntity;
+    const createdUser = await this.userRepository.createUser(userEntity);
+    return createdUser;
   }
 
   async updateUser(userData: PartialUserDto): Promise<IUserEntity> {
-    this.users.map((user, index) => {
-      if (user.id === userData.id) {
-        const updateUser = Object.assign(user, userData);
-        this.users.splice(index, 1, updateUser);
-      }
-    });
-    const updateUser = this.users.find((user) => user.id === userData.id);
+    const updateUser = await this.userRepository.updateUser(userData);
     return updateUser;
   }
 
   async getAllUsers(): Promise<IUserEntity[]> {
-    return this.users;
+    return await this.userRepository.findAllUsers();
   }
 
   async deleteUserById(userId: string): Promise<boolean> {
-    const existUser = this.users.find((user) => user.id === userId);
-    if (!existUser) {
+    try {
+      await this.userRepository.deleteUser(userId);
+      return true;
+    } catch (err) {
+      console.log(err);
       return false;
     }
-    this.users.map((user, index) => {
-      if (user.id === userId) {
-        this.users.splice(index, 1);
-      }
-    });
-    return true;
   }
 
   async getUserById(userId: string): Promise<IUserEntity> {
-    const existUser = this.users.find((user) => user.id === userId);
-    if (!existUser) {
-      throw new Error('User not found');
-    }
-    return existUser;
+    const foundUser = await this.userRepository.findUserById(userId);
+    return foundUser;
   }
 }
